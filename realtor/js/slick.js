4a18b27bedaf03a905d008bfb5f516b47e32d909
Kirill <krezna117@gmail.com>
2021-05-14 21:35:03 +0300

    добавлены версии на 1024 и 768 пикселей

--- realtor/js/slick.js
+++ realtor/js/slick.js
@@ -4,6 +4,38 @@ $(document).ready(function() {
     slidesToShow: 3,
     slidesToScroll: 1,
     infinite: true,
-  });
-})
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2.5
+        }
+      },
+
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2.5,
+          arrows: false
+        }
+      },
+
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        }
+      },
+
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1.5,
+          arrows: false
+        }
+      }
+    ]
+  })
+});
 
